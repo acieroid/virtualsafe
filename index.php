3d2f4bd59af64e24fa61509f7baf006abc8c279b
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-11-30 08:43:37 +0100

    try to connect to the db

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php echo "foo"; ?>
+<?php
+$con = mysql_connect('localhost','secu','secu');
+if (!$con) {
+  die('Could not connect: ' . mysql_error());
+}
+echo 'Connected to the DB';
+mysql_close($con);
+?>
