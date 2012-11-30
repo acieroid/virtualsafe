01e62b0905ddf21a9a191d98c4bded453d4e3140
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-11-30 22:25:49 +0100

    adding sql schema

---
+++ schema.sql
@@ -0,0 +1,35 @@
+create table admin(
+       id integer not null auto_increment,
+       -- the login name
+       name varchar(100),
+       -- SHA-256 hash
+       password varchar(32),
+       primary key(id))
+
+create table user(
+       id integer not null auto_increment,
+       -- the login name
+       -- the user certificate is stored in md5(name)/certificate.crt
+       name varchar(100),
+       -- SHA-256 hash
+       password varchar(32), 
+       -- is this user already validated?
+       valid boolean,
+       primary key(id))
+
+create table file(
+       id integer not null auto_increment,
+       -- the owner (creator) of this file
+       owner_id integer,
+       -- the user who can read this file
+       user_id integer,
+       -- the name of the file (user point of view)
+       -- on the server, the file is stored in
+       -- md5(username)/md5(filename) and the signature in
+       -- md5(username)/md5(filename).sign
+       filename varchar(100),
+       primary key(id),
+       foreign key (owner_id) references user(id) on delete cascade,
+       foreign key (user_id) references user(id) on delete cascade)
+         
+       
