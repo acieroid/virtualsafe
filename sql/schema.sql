742af6f276b620a225e5e2560b8befcb962235d6
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-12-07 09:28:17 +0100

    updating schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -13,6 +13,8 @@ create table user(
        id integer not null auto_increment,
        -- the login name
        -- the user certificate is stored in md5(name)/certificate.crt
+       -- TODO: we might need two pairs of keys, one for the
+       -- certificate and one for encrypting the files (why?)
        name varchar(100),
        -- SHA-256 hash
        password varchar(256), 
@@ -23,16 +25,26 @@ create table user(
        primary key(id));
 
 create table file(
-       id integer not null auto_increment,
-       -- the owner (creator) of this file
-       owner_id integer,
-       -- the user who can read this file
+       id integer not null auto_increment
+       -- the owner of this file
        user_id integer,
        -- the name of the file (user point of view)
        -- on the server, the file is stored in
        -- md5(username)/md5(filename) and the signature in
        -- md5(username)/md5(filename).sign
+       -- the file is encrypted with a random secret key, which is
+       -- stored encrypted in md5(username)/md5(filename).key
        filename varchar(100),
-       primary key(id),
+       primary key(id)
+       foreign key (user_id) references user(id) on delete cascade);
+
+create table share(
+       -- the file
+       file_id integer,
+       -- the owner of this file
+       owner_id integer,
+       -- the user who can read this file
+       user_id integer,
+       foreign key (file_id) references file(id) on delete cascade,
        foreign key (owner_id) references user(id) on delete cascade,
        foreign key (user_id) references user(id) on delete cascade);
