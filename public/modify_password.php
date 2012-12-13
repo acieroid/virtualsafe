c614d8fea46166c711d0bdb6dc66796f99854a04
vadim <mistervad@hotmail.com>
2012-12-13 15:14:25 +0100

    super

--- public/modify_password.php
+++ public/modify_password.php
@@ -9,18 +9,32 @@ if (!session_has_user()) {
   <p>Please <a href="signin.php">sign in</a></p>
  <?php
  }
-else if (isset($_GET['change_password'], $_POST['new_password'])) {
+else if (isset($_GET['change_password'], $_POST['new_password1'], $_POST['new_password2'])) {
   /* The user wants to change his password */
   $user = session_get_user();
-  if ($user->change_password($_POST['new_password'])) {  
+  if (strcmp($_POST['new_password1'], $_POST['new_password2'])==0){//Compare the new password
+	if ($user->change_password($_POST['new_password1'])) {  
 ?>
-	<p>Password changed</p>
+		<p>Password changed</p>
 <?php
-}} else{
+}	else{
+?>
+	<p>Fail to change password</p>
+	<?php
+	
+}} 
+	else {
+	?>
+			<p>The Passwords do not much</p>
+<?php
+}
+	}
+	else{
 ?>
 <form action="modify_password.php?change_password" method="post">
   <p><label for="old_password">Old Password: </label><input type="password" name="old_password" id="old_password"/></p>
-  <p><label for="new_password">New Password: </label><input type="password" name="new_password" id="new_password"/></p>
+  <p><label for="new_password1">New Password: </label><input type="password" name="new_password1" id="new_password1"/></p>
+  <p><label for="new_password2">New Password Again: </label><input type="password" name="new_password2" id="new_password2"/></p>
   <p><input type="submit" value="Change My Password" /></p>
 </form>
 <?php
