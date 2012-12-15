fcb38c252f4bb4a284f40692621e91281e545377
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-12-15 19:56:19 +0100

    correction du chemin de la clé pour les fichier partagés

--- public/download_shared.php
+++ public/download_shared.php
@@ -31,10 +31,15 @@ if (!session_has_user()) {
 } else if (isset($_GET['name'], $_GET['user'], $_GET['key'])) {
   /* Download the key. It is stored in the user's directory */
   $user = session_get_user();
+  $owner = User::find(urldecode($_GET['user']));
+  if ($owner == null) {
+    die('No such user');
+  }
   $name = urldecode($_GET['name']);
+  /* take the key of the user, not the owner */
   $key = $user->get_key_path($name);
 
-  if ($user->has_file($name) && file_exists($key)) {
+  if ($user->has_access($owner, $name) && file_exists($key)) {
     header('Content-Description: File Transfer');
     header('Content-Disposition: attachment; filename="' . $name . '.key"');
     header("Content-type: application/octet-stream");
@@ -54,7 +59,7 @@ if (!session_has_user()) {
   $name = urldecode($_GET['name']);
   $signature = $owner->get_signature_path($name);
 
-  if ($user->has_access($name) && file_exists($signature)) {
+  if ($user->has_access($owner, $name) && file_exists($signature)) {
     header('Content-Description: File Transfer');
     header('Content-Disposition: attachment; filename="' . $name . '.sign"');
     header("Content-type: application/octet-stream");
@@ -74,7 +79,7 @@ if (!session_has_user()) {
   $name = urldecode($_GET['name']);
   $certificate = $owner->get_certificate_file($name);
 
-  if ($user->has_access($name) && file_exists($certificate)) {
+  if ($user->has_access($owner, $name) && file_exists($certificate)) {
     header('Content-Description: File Transfer');
     header('Content-Disposition: attachment; filename="' . $name . '.crt"');
     header("Content-type: application/octet-stream");
@@ -91,10 +96,10 @@ if (!session_has_user()) {
 
   <p>Download the following files:</p>
   <ul>
-  <li><a href="download.php?name=<?php echo $_GET['name']; ?>&user=<?php echo $_GET['user']; ?>&encrypted">The encrypted file</a></li>
-  <li><a href="download.php?name=<?php echo $_GET['name']; ?>&user=<?php echo $_GET['user']; ?>&key">The key</a></li>
-  <li><a href="download.php?name=<?php echo $_GET['name']; ?>&user=<?php echo $_GET['user']; ?>&signature">The signature</a></li>
-  <li><a href="download.php?name=<?php echo $_GET['name']; ?>&user=<?php echo $_GET['user']; ?>&certificate">The certificate</a></li>  
+  <li><a href="download_shared.php?name=<?php echo $_GET['name']; ?>&user=<?php echo $_GET['user']; ?>&encrypted">The encrypted file</a></li>
+  <li><a href="download_shared.php?name=<?php echo $_GET['name']; ?>&user=<?php echo $_GET['user']; ?>&key">The key</a></li>
+  <li><a href="download_shared.php?name=<?php echo $_GET['name']; ?>&user=<?php echo $_GET['user']; ?>&signature">The signature</a></li>
+  <li><a href="download_shared.php?name=<?php echo $_GET['name']; ?>&user=<?php echo $_GET['user']; ?>&certificate">The certificate</a></li>  
   </ul>
   <p>Then run <pre>java -jar signer.jar -d file_in key.key file_out</pre> to decrypt the file, and <pre>java -jar signer.jar -c file_out signature.sign certificate.crt</pre> to check that the file matches the signature.</p>
 <?php
