4469685ca5c3f1dda81b6d582eb7c11fcc7faaff
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-12-15 10:01:16 +0100

    fix some errors in upload

--- public/upload.php
+++ public/upload.php
@@ -8,25 +8,25 @@ require_once('../include/csrf.php');
 include('menu.php');
 
 if (!session_has_user()) {
-?>
-<p>Please <a href="signin.php">sign in</a></p>
-<?php
-}
-else if(isset($_FILES['file'],$_FILES['signature']) {
-	if (CSRF::check($_POST['csrf'])) {
-	  if(($_FILES['file']['error'] == 0 || $_FILES['file']['error'] == UPLOAD_ERR_OK) &&
-	    ($_FILES['signature']['error'] == 0 || $_FILES['signature']['error'] == UPLOAD_ERR_OK)) {
-	    $user = session_get_user();
-	    $fileName = $_FILES['file']['name'];
-	    if($user->check_signature($_FILES['file']['tmp_name'],$_FILES['signature']['tmp_name']) {
-	      $user->encrypt_file($_FILES['file']['tmp_name'],$fileName);
-        $user->save_signature($_FILES['signature']['tmp_name'],$fileName);
+  ?>
+  <p>Please <a href="signin.php">sign in</a></p>
+  <?php
+} else if (isset($_FILES['file'], $_FILES['signature'])) {
+  if (CSRF::check($_POST['csrf'])) {
+    if (($_FILES['file']['error'] == 0 || $_FILES['file']['error'] == UPLOAD_ERR_OK) &&
+        ($_FILES['signature']['error'] == 0 || $_FILES['signature']['error'] == UPLOAD_ERR_OK)) {
+      $user = session_get_user();
+      $fileName = $_FILES['file']['name'];
+      if ($user->check_signature($_FILES['file']['tmp_name'], $_FILES['signature']['tmp_name'])) {
+        $user->encrypt_file($_FILES['file']['tmp_name'], $fileName);
+        $user->save_signature($_FILES['signature']['tmp_name'], $fileName);
         $user->add_file($fileName);
         echo '<p>File has been added</p>';
       } else {
         echo '<p>The signature is invalid</p>';
       }
     } else {
+      echo '<p>Error when uploading the files</p>';
     }
   } else {
     echo '<p>The CSRF token is invalid</p>';
@@ -34,13 +34,13 @@ else if(isset($_FILES['file'],$_FILES['signature']) {
 }
 else {
   $token = CSRF::generate();
-?>
-<form method="post" action="upload.php" enctype="multipart/form-data">
-	<p><label for="file">File to upload: </label><input type="file" name="file" id="file"/></p>
-	<p><label for="signature">Signature: </label><input type="text" name="signature" id="signature"/></p>
-	<p><input type="submit" value="Send"></p>
-	<?php echo '$token->get()'; ?>
-</form>
-<?php
+  ?>
+  <form method="post" action="upload.php" enctype="multipart/form-data">
+  <p><label for="file">File to upload: </label><input type="file" name="file" id="file"/></p>
+  <p><label for="signature">Signature: </label><input type="file" name="signature" id="signature"/></p>
+  <?php echo $token->get(); ?>
+  <p><input type="submit" value="Send"></p>
+  </form>
+  <?php
 }
 ?>
