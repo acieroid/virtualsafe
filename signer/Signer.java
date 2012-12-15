a5f2f5927c1ddbdf1970593062d937bfaab5b67d
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-12-15 12:18:53 +0100

    meilleure façon de demander la clé à l'utilisateur de l'app java

--- signer/Signer.java
+++ signer/Signer.java
@@ -28,8 +28,9 @@ public class Signer {
 
         if (!manager.keysExists()) {
             ui.askKeys(manager);
+        } else {
+            ui.run(manager);
         }
-        ui.run(manager);
     }
 
     /**
@@ -39,11 +40,11 @@ public class Signer {
         System.out.println("Usage: signer OPTIONS...");
         System.out.println("Available options:");
         System.out.println("\t[-h|--help]\n\t\tDisplay this help");
+        System.out.println("\t[-n]\n\t\tAsk the user for a new certificate. First thing to do when a new user is created");
         System.out.println("\t[-s file signature]\n\t\tSign 'file', storing the signature in 'signature'");
         System.out.println("\t[-c file signature [certificate]]\n\t\tCheck the signature of a file, with a given certificate (or the user's certificate if none specified");
         System.out.println("\t[-e file_in file_out keyfile]\n\t\tEncrypt a file, storing the result in 'file_out' and the key used, encrypted with the user's public key in 'keyfile'");
         System.out.println("\t[-d file_in file_out keyfile]\n\t\tDecrypt a file, storing the result in 'file_out', using the encrypted secret key from 'key_file'");
-        System.out.println("\t[-n]\n\t\tAsk the user for a new certificate");
         System.out.println("\t[--share key_in key_out]\n\t\tDecrypt the key in 'key_in', re-encrypt it with another user public-key (asked interactively) and saves it to 'key_out'");
     }
 }
