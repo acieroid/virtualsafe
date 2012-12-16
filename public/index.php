785306f670141ff7ce880dc325fb0a9e08b33dc6
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-12-16 20:14:45 +0100

    màj du document d'installation, ajout d'un lien pour dwl le programme

--- public/index.php
+++ public/index.php
@@ -5,5 +5,6 @@ require_once('../include/model.php');
 require_once('../include/sessions.php');
 require_once('../include/csrf.php');
 include('menu.php');
-
 ?>
+
+<p>Need the program? Download it from <a href="signer.jar">here</a>.</p>
