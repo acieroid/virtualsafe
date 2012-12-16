920b07f9ce22262b71ee7d913943520129cf19e7
vadim <mistervad@hotmail.com>
2012-12-16 13:41:12 +0100

    hou

--- public/unshare.php
+++ public/unshare.php
@@ -8,5 +8,15 @@ include('menu.php');
 
 if (!session_has_user()) {
   echo '<p>Please <a href="signin.php">sign in</a></p>';
-
+  }
+else {
+?>
+	<p> Which File do you want to unshare?</p>;
+<?php
+	$user = session_get_user();
+	$files = $user->list_shared_files();
+	//Affiche l'entièreté des fichiers partagé
+	?>
+<?php
+}
 ?>
